2958c246e78bdd1f5e07349518a496b0c62e4626
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-04-12 09:13:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "Sukaina is the champion";
+document.body.appendChild(newHeader);
